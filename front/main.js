6ffa9b8fd5040ffcd231ca337d074bad158bc1d5
landroide13 <landrade1313@gmail.com>
2021-11-06 21:29:58 +1300

    finish

--- front/main.js
+++ front/main.js
@@ -1,25 +1,31 @@
+function activate() {
+  $("#btn").fadeIn();
+}
+
+function showTable() {
+  $("#t1").removeClass("not");
+}
 
 $("#btn").click(function () {
-  $("#room").load("rooms.txt", function (responseTxt, statusTxt, xhr) {
-    if (statusTxt == "success");
-    if (statusTxt == "error")
-      alert("Error: " + xhr.status + ": " + xhr.statusText);
-  });
+  $("#room1").load("./resources/rooms.html #r1");
+  $("#room2").load("./resources/rooms.html #r2");
+  $("#room3").load("./resources/rooms.html #r3");
+
+  $("#type1").load("./resources/rooms.html #t1");
+  $("#type2").load("./resources/rooms.html #t2");
+  $("#type3").load("./resources/rooms.html #t3");
+
+  $("#out1").load("./resources/rooms.html #o1");
+  $("#out2").load("./resources/rooms.html #o2");
+  $("#out3").load("./resources/rooms.html #o3");
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+$("#form1").submit(function (event) {
+  event.preventDefault();
+  showTable();
+});
 
 
 
